5aca018da1f17866cf0b743408ab3b63eb6e9764
Omprakash Khawshi <80743848+ok97@users.noreply.github.com>
2021-04-22 16:40:51 +0530

    Added: Respond Happy or Sad Mood

--- MSTestForMoodAnalyzer/UnitTest1.cs
+++ MSTestForMoodAnalyzer/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using MoodAnalyzerProblem;
 
@@ -6,7 +6,25 @@ namespace MSTestForMoodAnalyzer
 {
     [TestClass]
     public class UnitTest1  //Class
-    {
+     /* TC 1.1:- Given “I am in Sad Mood” message Should Return SAD
+                    - analyseMood method can just return SAD to pass this Test Case (TC)
+        */
+        [TestMethod]
+        public void AnalyseMood_ShouldReturn_Sad()       
+        {
+            //Arraneg
+            string expected = "sad";
+            MoodAnalyzer obj = new MoodAnalyzer("I am in sad Mood");
+            //Act
+            string actual = obj.Analyzer();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /* TC 1.2:- Given “I am in Any Mood” message Should Return HAPPY.
+                    - To make the Test case pass analyseMood method need to check for Sad else return HAPPY
+       */
         [TestMethod]
         public void Given_Happymood_Expecting_Happy_Result()  //Method
         {
@@ -15,19 +33,6 @@ namespace MSTestForMoodAnalyzer
 
             string actual = mood.Analyzer();    //act
             Assert.AreEqual(expected, actual);  //Assert
-
-
-        }
-
-        public void Given_Happymood_Expecting_Sad_Result()  //Method
-        {
-            MoodAnalyzer mood = new MoodAnalyzer("I am in sad mood"); //Create object and arrange 
-            string expected = "sad";
-
-            string actual = mood.Analyzer();    //act
-            Assert.AreEqual(expected, actual);  //Assert
-
-
         }
     }
 }
