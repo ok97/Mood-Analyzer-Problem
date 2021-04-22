54aa12ecde6c77c960aadf61b88a42226f6b0e89
Omprakash Khawshi <80743848+ok97@users.noreply.github.com>
2021-04-22 19:03:06 +0530

    Added: Use Reflection Parameter Constructor

---
+++ MoodAnalyzerProblem/MoodAnalyserFactory.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Lucene.Net.Analysis;
+using System.Reflection;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzerFactory
+    {/* UC4:- create default Constructor - Use Reflection to Create MoodAnalyser with default Constructor
+              - Create MoodAnalyserFactory and specify static method to create MoodAnalyser Objec
+              MoodAnalyserObject to create MoodAnalyser object 
+        */
+        public object CreateMoodAnalyzerObject(string className, string constructor)
+        {
+            //MoodAnalyzerProblem.MoodAnalyzer
+            string pattern = @"." + constructor + "$";
+            Match result = Regex.Match(className, pattern); //regex predefine class .pattern matching store result suppose pattern matching then create object than an constructor
+
+            if (result.Success) //success boolen property sucess hold true value other wise false //classma,e and patytern both are matching
+            {
+                try
+                {    //constructor and classname both are matching
+
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    Type moodAnalyzerType = assembly.GetType(className);
+                    var res = Activator.CreateInstance(moodAnalyzerType); //Activator class
+                    return res;
+                }
+                catch (NullReferenceException)
+                {
+                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.CLASS_NOT_FOUND, "class not found");//class name not maching that time we run
+                }
+            }
+            else
+            {
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.CONSTRUCTOR_NOT_FOUND, "constructor not found");//Constructor name not maching that time we run
+            }
+        }
+
+        /* UC5:- create Parameter Constructor Use Reflection to Create MoodAnalyser with Parameter Constructor.
+                 - Use MoodAnalyserFactory to create MoodAnalyser Object with Message Parameneter.
+         */
+        public Object CreateMoodAnalyzerParameterObject(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyzer);
+
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo constructorObject = type.GetConstructor(new[] { typeof(string) });
+                    Object instance = constructorObject.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.CLASS_NOT_FOUND, "Class not found");
+
+            }
+        }
+
+
+        //public object CreateMoodAnalyzerParameterObject(string className, string constructor, string message)
+        //{
+        //    Type type = typeof(MoodAnalyzer);
+
+        //    if (type.Name.Equals(className) || type.FullName.Equals(className))  //check class naem same or not
+        //    {
+        //        if (type.Name.Equals(constructor))
+        //        {
+        //            //MoodAnalyzerProblem.MoodAnalyzer
+        //            //string pattern = @"." + constructor + "$";
+        //            // Match result = Regex.Match(className, pattern); //regex predefine class .pattern matching store result suppose pattern matching then create object than an constructor
+
+        //            ConstructorInfo constructorInfo = type.GetConstructor(new[] { typeof(string) }); //get constructor fetching one construct bsae on the  he feth string parameter constructor fetching 
+        //            var obj = constructorInfo.Invoke(new object[] { message }); //create object that Parameter Constructor by passing message
+        //            return obj;
+        //        }
+        //        else
+        //        {
+        //            throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.CONSTRUCTOR_NOT_FOUND, "constructor not found");//Constructor name not maching that time we run
+        //        }
+        //    }
+        //    else
+        //    {
+        //        throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.CLASS_NOT_FOUND, "class not found");//Constructor name not maching that time we run
+        //    }
+        //}
+
+        /* UC6:- Use Reflection to invoke Method –analyseMood 
+                 - Use Reflector to Invoke Method
+        */
+        public string InvokeAnalyzerMethod(string message, string methodName)
+        {
+            try
+            {
+                Type type = typeof(MoodAnalyzer);
+                               
+                MethodInfo analyzerMoodInfo = type.GetMethod(methodName);
+                MoodAnalyzerFactory Factory = new MoodAnalyzerFactory();
+               object moodAnalyzerObject = Factory.CreateMoodAnalyzerParameterObject("MoodAnalyzerProblem.MoodAnalyzer", "MoodAnalyzer", message);
+                object mood = analyzerMoodInfo.Invoke(moodAnalyzerObject, null);
+                return mood.ToString();
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_METHOD, "Method is not found");
+            }
+        }
+
+        
+
+
+        /* UC7:- Use Reflection to change mood dynamicall
+                 - User Reflector to Modify mood dynamically
+        */
+
+        //public static string SetField(string message, string fieldName)
+        //{         
+
+        //        MoodAnalyzerFactory Fact = new MoodAnalyzerFactory();
+        //        MoodAnalyzer obj = (MoodAnalyzer)Fact.CreateMoodAnalyzerObject("MoodAnalyzerProblem.MoodAnalyzer", "MoodAnalyzer");
+        //        Type type = typeof(MoodAnalyzer);
+        //        FieldInfo field = type.GetField(fieldName, BindingFlags.Public | BindingFlags.Instance);
+        //        if (field!= null)
+        //        {
+        //            if (message==null)
+        //            {
+        //                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL_MESSAGE, "Message should not be null");
+        //            }
+        //            field.SetValue(obj, message);
+        //            return obj.message;
+        //        }
+        //        throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.FIELD_NULL, "FieldName should not be null");
+        //}
+
+        /* UC7:- Use Reflection to change mood dynamically 
+                 - User Reflector to Modify mood dynamicall
+        */
+        public static string SetField(string message, string fieldName)
+        {
+            try
+            {
+                if (message == null)
+                {
+                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL, "Mood should not be NULL");
+                }
+
+                MoodAnalyzer obj = new MoodAnalyzer();
+
+                Type type = Type.GetType("MoodAnalyzerProblem.MoodAnalyzer");
+
+                FieldInfo field = type.GetField(fieldName);
+
+                field.SetValue(obj, message);
+
+                return obj.message;
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_FIELD, "No Such Field");
+            }
+
+        }
+    }
+}
---
+++ MoodAnalyzerProblem/MoodAnalyzerFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzerFactory
+    {
+
+        public static Object CreateMoodAnalyse(string className, string constructorName)
+        {
+            String pattern = @"." + constructorName + "$";
+            //Regex regex = new Regex(pattern);
+            Match result = Regex.Match(className, pattern);
+
+            if (result.Success)
+            {
+
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
+                }
+            }
+            else
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
+
+            }
+
+
+        }
+
+
+    }
+}
